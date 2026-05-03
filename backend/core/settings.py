8697b6445c97e05ae201b3266b9e35062472c543
soroush eghdami <soroush.egh@gmail.com>
2026-05-03 19:40:02 +0330

    FIXED THA FUCKING DOCKER

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'drf_spectacular_sidecar',
     'rest_framework_simplejwt.token_blacklist',
-    'corsheaders'
+    'corsheaders',
     # Local apps
     'accounts',
     'tweets',
